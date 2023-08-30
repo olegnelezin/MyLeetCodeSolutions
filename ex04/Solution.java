7af0cfc55210498a20bf8d3fa641c7eb80f781b5
Олег Нелезин <olegnelezin@MacBook-Pro-Oleg.local>
2023-08-30 22:16:29 +0300

    add ex03 & ex04

---
+++ ex04/Solution.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int c = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+           char a = jewels.charAt(i);
+            for (int j = 0; j < stones.length(); j++) {
+                if (stones.charAt(j) == a) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}

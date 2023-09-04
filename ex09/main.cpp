bd5f0d65f830e005295c96bbdd445dc0a5d87c29
Олег Нелезин <olegnelezin@MacBook-Pro-Oleg.local>
2023-09-04 21:59:44 +0300

    add ex09

---
+++ ex09/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> temp;
+        for (int i = 0; i < n; ++i) {
+            temp.push_back(nums[i]);
+            temp.push_back(nums[i+n]);
+        }
+        return temp;
+    }
+};

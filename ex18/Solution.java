1e3f4f154ee82be766cba00ae6d9914f7195c801
olegnelezin <astravsu@gmail.com>
2023-09-14 21:59:52 +0300

    add ex18

---
+++ ex18/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j] && i != j) {
+                    count++;
+                }
+            }
+            a[i] = count;
+        }
+        return a;
+    }
+
+}
